55d39d83129a02d2951c035679301138ccffd537
nabil <merchant.nabil@gmail.com>
2022-01-07 20:31:03 -0600

    Testing decorator functions

--- hello.py
+++ hello.py
@@ -5,13 +5,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1 style="text-align: center">Hello World!</h1>' \
+           '<p>This is a paragraph.</p>' \
+           '<img src="https://64.media.tumblr.com/tumblr_ly9h3l7eSW1r31cpxo1_500.gif">'
+
+
+@app.route('/<name>')
+def greet(name):
+    return f"Hello there {name}!"
+
+
+def make_bold(function):
+    def wrapper():
+        return f'<b>{function()}</b>'
+
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return f'<em>{function()}</em>'
+
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper():
+        return f'<u>{function()}</u>'
+
+    return wrapper
 
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underline
 def say_bye():
-    return "Bye"
+    return "Bye!"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
